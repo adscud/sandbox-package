d7a495f1fcc5a77141b61493d01fb1151cc8579b
adscud <arthur.delbeke@smartness.co>
2022-05-13 10:46:43 +0200

    chore: test

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,12 @@
+import { View } from 'react-native';
+import React from 'react';
+
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
+}
+
+export function User() {
+  return (
+    <View style={{ flex: 1, backgroundColor: 'red' }} />
+  );
 }
