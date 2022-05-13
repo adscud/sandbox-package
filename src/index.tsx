fb4b99c7d0ca078cf1102b073a65691a65473dfc
adscud <arthur.delbeke@smartness.co>
2022-05-13 11:09:32 +0200

    chore: add navigation event

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import React from 'react';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
+interface Props {
+  navigation: NativeStackScreenProps<any>['navigation'];
 }
 
-export function User() {
+export function User({ navigation }: Props) {
   return (
-    <View style={{ flex: 1, backgroundColor: 'red' }} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
+      <Button title={'ToProfile'} onPress={() => navigation.push('Account')} />
+    </View>
   );
 }
